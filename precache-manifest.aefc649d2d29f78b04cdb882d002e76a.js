b1ab00b99720530987b2da1626c93db67367a472
Dong Nguyen <dongnt98.it@gmail.com>
2022-06-24 21:27:09 +0700

    Updates

---
+++ precache-manifest.aefc649d2d29f78b04cdb882d002e76a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9787d6f1f6623df6a6a825a13ebe3e50",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "916d9649574758e9e1c5",
+    "url": "http://localhost:3000/static/css/main.8b3bcc81.chunk.css"
+  },
+  {
+    "revision": "e279f13c23b78fcaed8f",
+    "url": "http://localhost:3000/static/js/2.3c28594f.chunk.js"
+  },
+  {
+    "revision": "916d9649574758e9e1c5",
+    "url": "http://localhost:3000/static/js/main.d6fd5915.chunk.js"
+  },
+  {
+    "revision": "e8d955eac6f650306bb5",
+    "url": "http://localhost:3000/static/js/runtime~main.8a6cbfe7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3001/precache-manifest.b51c68d61afc90efe7d7246593f3ecc1.js"
+  "http://localhost:3000/precache-manifest.aefc649d2d29f78b04cdb882d002e76a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3001/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
